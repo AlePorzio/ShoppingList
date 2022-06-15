6ccef0a543efb753eca8d744e1c0b0f8320c3dcf
Alessandro <alenet1000@gmail.com>
2022-06-15 08:44:24 +0200

    Added Functions

--- ShoppingList.h
+++ ShoppingList.h
@@ -5,12 +5,36 @@
 #ifndef SHOPPINGLIST_SHOPPINGLIST_H
 #define SHOPPINGLIST_SHOPPINGLIST_H
 
+#include <list>
+#include <memory>
+#include "Subject.h"
 #include "Observer.h"
+#include "Product.h"
+using namespace std;
 
-class ShoppingList : public Observer {
+class ShoppingList : public Subject {
 
+private:
 
+    list<unique_ptr<Observer>> observers;
+    list<unique_ptr<Product>> products;
 
+public:
+
+    virtual void addProduct(unique_ptr<Product> p){
+        products.push_back(move(p));
+        notifyObserver();
+    }
+
+    virtual void removeProduct(unique_ptr<Product> p){
+        products.remove(p);
+        notifyObserver();
+    }
+
+    void notifyObserver() override{
+        for(auto const& o : observers)
+            o->update();
+    }
 };
 
 
